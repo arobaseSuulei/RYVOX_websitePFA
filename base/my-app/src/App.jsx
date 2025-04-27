8a6bddacada69a9e6ed6d265903ae2c0e2274d22
arobaseSuulei <souleymanediallo2004@outlook.com>
2025-04-27 19:51:41 +0100

    router ok

--- base/my-app/src/App.jsx
+++ base/my-app/src/App.jsx
@@ -1,7 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {useEffect} from "react";
+import Home from "./components/Home.jsx";
+
 export default function App() {
+
+
+    useEffect(() => {
+        document.title = "Ryvox";
+    }, []);
+
+
+
     return(
-        <div className={'bg-red-600'}>
-            hey
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </Router>
     );
 }
